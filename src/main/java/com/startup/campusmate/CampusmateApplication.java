d755b5d255682a58621db0e2cb177ba1cfb83f44
joon0890 <kimjunhee2483@gmail.com>
2025-07-08 22:30:57 +0900

    Start: 스프링 부트 시작

---
+++ src/main/java/com/startup/campusmate/CampusmateApplication.java
@@ -0,0 +1,13 @@
+package com.startup.campusmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startup/campusmate/CampusmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.campusmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
