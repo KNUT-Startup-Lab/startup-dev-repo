3a17b89474cfefe2234046034efdcf28b4eadaa4
joon0890 <kimjunhee2483@gmail.com>
2025-07-31 19:58:24 +0900

    refactor: SecurityConfig.java 수정

--- src/main/java/com/startup/campusmate/global/security/SecurityConfig.java
+++ src/main/java/com/startup/campusmate/global/security/SecurityConfig.java
@@ -1,62 +1,96 @@
 package com.startup.campusmate.global.security;
 
+import com.startup.campusmate.global.security.member.CustomMemberDetailsService;
 import com.startup.campusmate.global.security.oauth2.CustomOAuth2FailureHandler;
 import com.startup.campusmate.global.security.oauth2.CustomOAuth2SuccessHandler;
 import com.startup.campusmate.global.security.oauth2.CustomOAuth2UserService;
 import com.startup.campusmate.global.security.oauth2.CustomOidcUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
-    private String kakaoClientSecret;
 
     private final CustomOidcUserService customOidcUserService;
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomOAuth2SuccessHandler customOAuth2SuccessHandler;
     private final CustomOAuth2FailureHandler customOAuth2FailureHandler;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private final CustomMemberDetailsService customMemberDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                 "/", "/login/**", "/api/auth/login", "/api/users",
-                                "/oauth2/**", "/api/auth/social/**").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(
+                        authorizeRequests ->
+                        {
+                            authorizeRequests
+                                    .requestMatchers("/")
+                                    .permitAll()
+                                    .requestMatchers("/login/**")
+                                    .permitAll()
+                                    .requestMatchers("/api/auth/login")
+                                    .permitAll()
+                                    .requestMatchers("/api/users")
+                                    .permitAll()
+                                    .requestMatchers("/oauth2/**")
+                                    .permitAll()
+                                    .requestMatchers("/api/auth/social/**")
+                                    .permitAll();
+                            authorizeRequests
+                                    .anyRequest().authenticated();
+                        }
                 )
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(logout -> logout.logoutUrl("/api/logout"));
-
-        http.oauth2Login(oauth2 -> oauth2
-                .userInfoEndpoint(userInfo -> userInfo
-                        .oidcUserService(customOidcUserService)       // 구글 등 OIDC
-                        .userService(customOAuth2UserService)        // 카카오 등 OAuth2
+                .oauth2Login(
+                        oauth2Login  ->
+                                oauth2Login
+                                        .userInfoEndpoint(userInfo -> userInfo
+                                                .oidcUserService(customOidcUserService)       // 구글 등 OIDC
+                                                .userService(customOAuth2UserService)        // 카카오 등 OAuth2
+                    )
+                    .successHandler(customOAuth2SuccessHandler)
+                    .failureHandler(customOAuth2FailureHandler)
                 )
-                .successHandler(customOAuth2SuccessHandler)
-                .failureHandler(customOAuth2FailureHandler)
-        );
+                .formLogin(
+                        form ->
+                                form
+                                        .loginPage("/login")  // 커스텀 로그인 페이지
+                                        .loginProcessingUrl("/process-login") // 로그인 요청 URL
+                                        .permitAll()
+                )
+                .logout(
+                        logout ->
+                                logout
+                                        .logoutUrl("/logout")
+                );
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(customMemberDetailsService)  // 여기 추가
+                .passwordEncoder(passwordEncoder())      // 비밀번호 인코딩 필수
+                .and()
+                .build();
+    }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // 보통 BCrypt 사용
+    }
 }
 
 
